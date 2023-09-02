565a65e14769c158d494375a75cef708d9f87e66
Kei Kunimasa <keikuni60@gmail.com>
2023-09-03 07:39:20 +0900

    まーじ用の更新

--- lib/pages/decide_page.dart
+++ lib/pages/decide_page.dart
@@ -12,39 +12,41 @@ class DecidePage extends StatelessWidget {
         title: const Text("decidePage"),
         backgroundColor: Theme.of(context).highlightColor,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Container(
-            height: 300,
-            child: const GoogleMap(
-              initialCameraPosition: CameraPosition(
-                  zoom: 17, //ズーム
-                  target: LatLng(35.0, 135.0), //経度,緯度
-                  tilt: 45.0, //上下の角度
-                  bearing: 90.0),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              height: 300,
+              child: const GoogleMap(
+                initialCameraPosition: CameraPosition(
+                    zoom: 17, //ズーム
+                    target: LatLng(35.0, 135.0), //経度,緯度
+                    tilt: 45.0, //上下の角度
+                    bearing: 90.0),
+              ),
             ),
-          ),
-          Container(
-            width: 300,
-            height: 100,
-            child: ElevatedButton(
-              onPressed: () {},
-              child: Text('外部サイトに飛ぶ'),
+            Container(
+              width: 300,
+              height: 100,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text('外部サイトに飛ぶ'),
+              ),
             ),
-          ),
-          Container(
-            width: 300,
-            height: 100,
-            margin: EdgeInsets.all(50),
-            child: ElevatedButton(
-              onPressed: () {
-                Get.toNamed(AppRoutes.home);
-              },
-              child: Text('最初に戻る'),
+            Container(
+              width: 300,
+              height: 100,
+              margin: EdgeInsets.all(50),
+              child: ElevatedButton(
+                onPressed: () {
+                  Get.toNamed(AppRoutes.home);
+                },
+                child: Text('最初に戻る'),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
