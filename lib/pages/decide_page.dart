787722f05c1bdd9ca6a9e4892b098323213c5d60
inumaruseiya <s.inumaru2023@gmail.com>
2023-09-02 18:35:25 +0900

    decide_pageのフロント(仮)

--- lib/pages/decide_page.dart
+++ lib/pages/decide_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class DecidePage extends StatelessWidget {
   const DecidePage({Key? key}) : super(key: key);
@@ -9,7 +10,38 @@ class DecidePage extends StatelessWidget {
         title: const Text("decidePage"),
         backgroundColor: Theme.of(context).highlightColor,
       ),
-      body: const Center(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            height: 300,
+            child: const GoogleMap(
+              initialCameraPosition: CameraPosition(
+                  zoom: 17, //ズーム
+                  target: LatLng(35.0, 135.0), //経度,緯度
+                  tilt: 45.0, //上下の角度
+                  bearing: 90.0),
+            ),
+          ),
+          Container(
+            width: 300,
+            height: 100,
+            child: ElevatedButton(
+              onPressed: () {},
+              child: Text('外部サイトに飛ぶ'),
+            ),
+          ),
+          Container(
+            width: 300,
+            height: 100,
+            margin: EdgeInsets.all(50),
+            child: ElevatedButton(
+              onPressed: () {},
+              child: Text('最初に戻る'),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
