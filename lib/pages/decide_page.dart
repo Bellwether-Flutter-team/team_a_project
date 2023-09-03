39022777f0f6ae6e6bd2f3dffc503219ac5670d1
inumaruseiya <s.inumaru2023@gmail.com>
2023-09-03 22:06:30 +0900

    完成仮

--- lib/pages/decide_page.dart
+++ lib/pages/decide_page.dart
@@ -3,82 +3,61 @@ import '../routes/app_routes.dart';
 import '../states/decide_page_controller.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:get/get.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class DecidePage extends StatelessWidget {
   const DecidePage({Key? key}) : super(key: key);
+  final url = 'f69100125abe9612';
   @override
   Widget build(BuildContext context) {
+    ShopDataController shopDataController = Get.put(ShopDataController());
     DecidePageController decidepagecontroller = Get.put(DecidePageController());
-    final latitude = decidepagecontroller.shopplacelatitude.value;
-    final longitude = decidepagecontroller.shopplacelongitude.value;
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(decidepagecontroller.shopname.value),
-        backgroundColor: Theme.of(context).highlightColor,
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Container(
-              height: 300,
-              child: GoogleMap(
-                initialCameraPosition: CameraPosition(
-                    zoom: 17, //ズーム
-                    target: LatLng(latitude, longitude), //経度,緯度
-                    tilt: 45.0, //上下の角度
-                    bearing: 90.0),
+    return Obx(
+      ()=> Scaffold(
+        appBar: AppBar(
+          title: Text(decidepagecontroller.shopname.value),
+          backgroundColor: Theme.of(context).highlightColor,
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                height: 300,
+                child: GoogleMap(
+                  initialCameraPosition: CameraPosition(
+                      zoom: 17, //ズーム
+                      target: LatLng(decidepagecontroller.shopplacelatitude.value, decidepagecontroller.shopplacelongitude.value), //経度,緯度
+                      tilt: 45.0, //上下の角度
+                      bearing: 90.0),
+                ),
               ),
-            ),
-            Container(
-              width: 300,
-              height: 100,
-              child: ElevatedButton(
-                onPressed: () {
-                  final url = Uri.parse(decidepagecontroller.targeturl.value);
-                  launchUrl(url);
-                },
-                child: Text('外部サイトに飛ぶ'),
+              Container(
+                width: 300,
+                height: 100,
+                child: ElevatedButton(
+                  onPressed: () {
+                    final url = Uri.parse(decidepagecontroller.targeturl.value);
+                    launchUrl(url);
+                  },
+                  child: Text('外部サイトに飛ぶ'),
+                ),
               ),
-            ),
-            Container(
-              width: 300,
-              height: 100,
-              margin: EdgeInsets.all(50),
-              child: ElevatedButton(
-                onPressed: () {
-                  Get.toNamed(AppRoutes.home);
-                },
-                child: Text('最初に戻る'),
+              Container(
+                width: 300,
+                height: 100,
+                margin: EdgeInsets.all(50),
+                child: ElevatedButton(
+                  onPressed: () {
+                    Get.toNamed(AppRoutes.home);
+                  },
+                  child: Text('最初に戻る'),
+                ),
               ),
-            ),
-            Container(
-              child: TextFormField(
-                decoration: const InputDecoration(hintText: 'お店の名前'),
-                onChanged: (value) {
-                  decidepagecontroller.shopname.value = value;
-                },
-              ),
-            ),
-            Container(
-              child: TextFormField(
-                decoration: const InputDecoration(hintText: 'お店のurl'),
-                onChanged: (value) {
-                  decidepagecontroller.targeturl.value = value;
-                },
-              ),
-            ),
-            Container(
-              child: TextFormField(
-                decoration: const InputDecoration(hintText: 'お店の緯度'),
-                onChanged: (value) {
-                  decidepagecontroller.shopplacelatitude.value =
-                      double as double;
-                },
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
--- lib/states/decide_page_controller.dart
+++ lib/states/decide_page_controller.dart
@@ -1,21 +1,107 @@
 import 'dart:convert';
-import '../pages/decide_page.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 
+class ShopInfo {
+  final String name;
+  final String address;
+  final String url;
+  final double latitude;
+  final double longitude;
+
+  ShopInfo({
+    required this.name,
+    required this.address,
+    required this.url,
+    required this.latitude,
+    required this.longitude,
+  });
+}
+
+Future<ShopInfo?> fetchShopInfo(String shopName, String shopAddress) async {
+  final String apiKey = 'f69100125abe9612';
+  final String baseUrl =
+      'https://webservice.recruit.co.jp/hotpepper/gourmet/v1/';
+  final String shopName = 'シュラスコ&ビアレストラン ALEGRIA AKASAKA インターシティAIR（アレグリア赤坂';
+  final String shopAddress = '東京都港区赤坂１-8-1 赤坂インターシティAIR 1F';
+  final Map<String, String> queryParams = {
+    'key': apiKey,
+    'name': shopName,
+    'address': shopAddress,
+  };
+
+  final Uri uri = Uri.parse('$baseUrl/shops/');
+  final Uri fullUri = uri.replace(queryParameters: queryParams);
+
+  final response = await http.get(fullUri);
+
+  if (response.statusCode == 200) {
+    final Map<String, dynamic> data = json.decode(response.body);
+    final List<dynamic> shops = data['results']['shop'];
+
+    if (shops.isNotEmpty) {
+      final shop = shops[0]; // 最初のお店情報を取得
+
+      final String name = shop['name'];
+      final String address = shop['address'];
+      final String url = shop['urls']['pc'];
+      final double latitude = double.tryParse(shop['lat']) ?? 0.0;
+      final double longitude = double.tryParse(shop['lng']) ?? 0.0;
+
+      return ShopInfo(
+        name: name,
+        address: address,
+        url: url,
+        latitude: latitude,
+        longitude: longitude,
+      );
+    }
+  }
+
+  return null; // お店が見つからない場合はnullを返す
+}
+
+class ShopDataController extends GetxController {
+  RxString shopUrl = ''.obs;
+  RxDouble shopLatitude = 0.0.obs;
+  RxDouble shopLongtitude = 0.0.obs;
+
+  Future getShopplaceUrl() async {
+    final shopName = 'シュラスコ&ビアレストラン ALEGRIA AKASAKA インターシティAIR（アレグリア赤坂）';
+    final shopAddress = '東京都港区赤坂１-8-1 赤坂インターシティAIR 1';
+
+    final shopInfo = await fetchShopInfo(shopName, shopAddress);
+    if (shopInfo != null) {
+      shopUrl.value = '${shopInfo.url}';
+      shopLatitude.value = shopInfo.latitude;
+      shopLongtitude.value = shopInfo.longitude;
+    } else {
+      print('お店が見つかりませんでした。');
+    }
+  }
+}
+
 class DecidePageController extends GetxController {
+  final ShopDataController shopDataController = Get.find<ShopDataController>();
+
   RxString shopname = 'まだ選択されていません'.obs;
   RxString shopaddress = ''.obs;
   RxDouble shopplacelatitude = 35.6961868.obs;
   RxDouble shopplacelongitude = 139.4463985.obs;
-  RxString targeturl = 'https://www.skylark.co.jp/gusto/'.obs;
+  RxString targeturl = 'https://store-info.skylark.co.jp/map/018874'.obs;
   String apikey = 'f69100125abe9612';
 
-  updatestate(String selectedShopName) {
-    shopname.value = selectedShopName;
+  getShopLatitude() {
+    return shopDataController.shopLatitude.value;
   }
 
-  updatestate1(String selectedShopName) {
-    targeturl.value = selectedShopName;
+  getShopLongitude() {
+    return shopDataController.shopLongtitude.value;
+  }
+
+  getShopUrl() async{
+    
+    print('shopDataController.shopUrl.value: ${shopDataController.shopUrl.value}');
+    return shopDataController.shopUrl.value;
   }
 }
